40c6ec9b1f557623fb5263f5739a73e9549ba4e7
renakuwa <renakuwa22@gmail.com>
2019-11-27 00:17:54 +0900

    問４

---
+++ Q4.rb
@@ -0,0 +1,28 @@
+a = 2
+
+case a
+when 1
+  p 'January'
+when 2
+  p 'Febrary'
+when 3
+  p 'March'
+when 4
+  p 'April'
+when 5
+  p 'May'
+when 6
+  p 'June'
+when 7
+  p 'July'
+when 8
+  p 'August'
+when 9
+  p 'September'
+when 10
+  p 'October'
+when 11
+  p 'November'
+when 12
+  p 'December'
+end
