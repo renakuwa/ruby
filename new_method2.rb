9066ccb3f577a8d817d818583072bd713bc5809c
renakuwa <renakuwa22@gmail.com>
2019-12-09 18:14:22 +0900

    6章のファイル追加

---
+++ new_method2.rb
@@ -0,0 +1,9 @@
+def calc_triangle_area(base, height)
+    base * height / 2
+end
+
+mybase = 12
+myheight = 3
+
+p calc_triangle_area(mybase, myheight)
+
---
+++ new_method3.rb
@@ -0,0 +1,6 @@
+p calc_triangle_area(10, 5)
+def calc_triangle_area(base, height)
+    base * height / 2
+end
+
+# 関数は定義してからでないと使えないから動作できない
